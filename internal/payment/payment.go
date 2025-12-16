d457e4241d4c3d0e7ab5b8d7dff4186b5c4c1c0b
q0wqex <lis@q0wqex.ru>
2025-12-16 11:52:49 +0300

    fix(moynalog): move receipt sending earlier in purchase processing
    Relocate Moynalog receipt sending logic to execute immediately after
    payment confirmation, before referral and subscription processing.
    This ensures receipts are sent earlier in the purchase flow.

--- internal/payment/payment.go
+++ internal/payment/payment.go
@@ -116,6 +116,25 @@ func (s PaymentService) ProcessPurchaseById(ctx context.Context, purchaseId int6
 	if err != nil {
 		return err
 	}
+	// Отправка чека в МойНалог только при оплате через Юкассу
+	slog.Info("Checking conditions for Moynalog receipt", "invoice_type", purchase.InvoiceType, "moynalog_client", s.moynalogClient != nil)
+	if purchase.InvoiceType == database.InvoiceTypeYookasa && s.moynalogClient != nil {
+		slog.Info("Attempting to send receipt to Moynalog", "purchase_id", utils.MaskHalfInt64(purchase.ID), "amount", purchase.Amount, "month", purchase.Month)
+		go func() {
+			err := s.sendReceiptToMoynalog(purchase)
+			if err != nil {
+				slog.Error("Error sending receipt to Moynalog", "error", err, "purchase_id", utils.MaskHalfInt64(purchase.ID))
+			} else {
+				slog.Info("Successfully sent receipt to Moynalog", "purchase_id", utils.MaskHalfInt64(purchase.ID))
+			}
+		}()
+	} else {
+		if purchase.InvoiceType != database.InvoiceTypeYookasa {
+			slog.Info("Not sending receipt to Moynalog - not a Yookasa invoice", "invoice_type", purchase.InvoiceType, "purchase_id", utils.MaskHalfInt64(purchase.ID))
+		} else if s.moynalogClient == nil {
+			slog.Error("Not sending receipt to Moynalog - client is nil", "purchase_id", utils.MaskHalfInt64(purchase.ID))
+		}
+	}
 
 	ctxReferee := context.Background()
 	referee, err := s.referralRepository.FindByReferee(ctxReferee, customer.TelegramID)
@@ -157,26 +176,6 @@ func (s PaymentService) ProcessPurchaseById(ctx context.Context, purchaseId int6
 			InlineKeyboard: s.createConnectKeyboard(refereeCustomer),
 		},
 	})
-
-	// Отправка чека в МойНалог только при оплате через Юкассу
-	slog.Info("Checking conditions for Moynalog receipt", "invoice_type", purchase.InvoiceType, "moynalog_client", s.moynalogClient != nil)
-	if purchase.InvoiceType == database.InvoiceTypeYookasa && s.moynalogClient != nil {
-		slog.Info("Attempting to send receipt to Moynalog", "purchase_id", utils.MaskHalfInt64(purchase.ID), "amount", purchase.Amount, "month", purchase.Month)
-		go func() {
-			err := s.sendReceiptToMoynalog(purchase)
-			if err != nil {
-				slog.Error("Error sending receipt to Moynalog", "error", err, "purchase_id", utils.MaskHalfInt64(purchase.ID))
-			} else {
-				slog.Info("Successfully sent receipt to Moynalog", "purchase_id", utils.MaskHalfInt64(purchase.ID))
-			}
-		}()
-	} else {
-		if purchase.InvoiceType != database.InvoiceTypeYookasa {
-			slog.Info("Not sending receipt to Moynalog - not a Yookasa invoice", "invoice_type", purchase.InvoiceType, "purchase_id", utils.MaskHalfInt64(purchase.ID))
-		} else if s.moynalogClient == nil {
-			slog.Error("Not sending receipt to Moynalog - client is nil", "purchase_id", utils.MaskHalfInt64(purchase.ID))
-		}
-	}
 
 	slog.Info("purchase processed", "purchase_id", utils.MaskHalfInt64(purchase.ID), "type", purchase.InvoiceType, "customer_id", utils.MaskHalfInt64(customer.ID))
 
